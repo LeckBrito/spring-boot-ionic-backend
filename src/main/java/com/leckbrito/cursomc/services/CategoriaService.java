21d60ae09b60a0f7c6e3362a89fbc1b509298098
Alexander Dias Brito <alexander.diasbrito@gmail.com>
2018-10-15 22:44:46 -0300

    Criando repository e service para Categoria

---
+++ src/main/java/com/leckbrito/cursomc/services/CategoriaService.java
@@ -0,0 +1,22 @@
+package com.leckbrito.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.leckbrito.cursomc.domain.Categoria;
+import com.leckbrito.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria buscar(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+	}
+	
+}
