bdef512097db55073c09c5d1f9f9455716220f6f
Alexander Dias Brito <alexander.diasbrito@gmail.com>
2018-10-18 22:38:19 -0300

    Estado e cidade

---
+++ src/main/java/com/leckbrito/cursomc/repositories/CidadeRepository.java
@@ -0,0 +1,11 @@
+package com.leckbrito.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.leckbrito.cursomc.domain.Cidade;
+
+@Repository
+public interface CidadeRepository extends JpaRepository<Cidade, Integer> {
+
+}
