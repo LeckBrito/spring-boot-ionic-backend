1b296472748a06a44427aecfc405ba61d0935d48
Alexander Dias Brito <alexander.diasbrito@gmail.com>
2018-10-20 14:24:17 -0300

    Endpoint /clientes/{id} disponível

---
+++ src/main/java/com/leckbrito/cursomc/services/ClienteService.java
@@ -0,0 +1,25 @@
+package com.leckbrito.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.leckbrito.cursomc.domain.Cliente;
+import com.leckbrito.cursomc.repositories.ClienteRepository;
+import com.leckbrito.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente buscar(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+				
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));						
+	}
+	
+}
