09a5d23998f47e889d96b29050483e958f00ed3f
Alexander Dias Brito <alexander.diasbrito@gmail.com>
2018-10-20 12:38:38 -0300

    Cliente, TipoCliente, telefones e enderecos

--- src/main/java/com/leckbrito/cursomc/CursomcApplication.java
+++ src/main/java/com/leckbrito/cursomc/CursomcApplication.java
@@ -9,10 +9,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.leckbrito.cursomc.domain.Categoria;
 import com.leckbrito.cursomc.domain.Cidade;
+import com.leckbrito.cursomc.domain.Cliente;
+import com.leckbrito.cursomc.domain.Endereco;
 import com.leckbrito.cursomc.domain.Estado;
 import com.leckbrito.cursomc.domain.Produto;
+import com.leckbrito.cursomc.domain.enums.TipoCliente;
 import com.leckbrito.cursomc.repositories.CategoriaRepository;
 import com.leckbrito.cursomc.repositories.CidadeRepository;
+import com.leckbrito.cursomc.repositories.ClienteRepository;
+import com.leckbrito.cursomc.repositories.EnderecoRepository;
 import com.leckbrito.cursomc.repositories.EstadoRepository;
 import com.leckbrito.cursomc.repositories.ProdutoRepository;
 
@@ -27,7 +32,11 @@ public class CursomcApplication implements CommandLineRunner {
 	private EstadoRepository estadoRepository;
 	@Autowired
 	private CidadeRepository cidadeRepository;
-	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	@Autowired
+	private EnderecoRepository enderecoRepository;
+	 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
@@ -64,5 +73,19 @@ public class CursomcApplication implements CommandLineRunner {
 		
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
+		
+		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "36378912377", TipoCliente.PESSOAFISICA);
+		
+		cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
+		
+		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 203", "Jardim", "38220834", cli1, c1);
+		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "38777012", cli1, c2);
+		
+		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
+		
+		clienteRepository.saveAll(Arrays.asList(cli1));
+		enderecoRepository.saveAll(Arrays.asList(e1, e2));
+			
+		
 	}
 }
