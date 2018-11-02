0a16488774eccabafa22894fcde727ce8f6566a0
Alexander Dias Brito <alexander.diasbrito@gmail.com>
2018-11-02 09:32:19 -0300

    Validação Customizada: email não repetido na atualização do Cliente

---
+++ src/main/java/com/leckbrito/cursomc/services/validation/ClienteUpdateValidator.java
@@ -0,0 +1,57 @@
+package com.leckbrito.cursomc.services.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerMapping;
+
+import com.leckbrito.cursomc.domain.Cliente;
+import com.leckbrito.cursomc.dto.ClienteDTO;
+import com.leckbrito.cursomc.repositories.ClienteRepository;
+import com.leckbrito.cursomc.resources.exceptions.FieldMessage;
+
+public class ClienteUpdateValidator implements ConstraintValidator<ClienteUpdate, ClienteDTO> {
+
+	@Autowired
+	private HttpServletRequest request;
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	@Override
+	public void initialize(ClienteUpdate ann) {
+	}
+
+	@Override
+	public boolean isValid(ClienteDTO objDto, ConstraintValidatorContext context) {
+		
+		//pegar o id que está na URI ( ele não vem no corpo do JSON )
+		@SuppressWarnings("unchecked")
+		Map<String, String> map = (Map<String, String>) request.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
+		Integer uriId = Integer.parseInt(map.get("id"));		
+		
+		//cria uma lista vazia de erros
+		List<FieldMessage> list = new ArrayList<>();
+		
+		// inclua os testes aqui, inserindo erros na lista		
+		
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		if (aux != null && !aux.getId().equals(uriId)) {
+			list.add(new FieldMessage("email", "Email já existente"));
+		}
+
+		//percorre nossa lista de erros e adiciona na lista de erros do framework (usado na ResourceExceptionHandler.java)
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+					.addConstraintViolation();
+		}
+		return list.isEmpty();
+	}
+}
