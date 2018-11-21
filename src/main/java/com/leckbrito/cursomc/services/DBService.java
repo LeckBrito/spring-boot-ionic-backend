531455e7305382503649871d3c1d3ed3acfce839
Alexander Dias Brito <alexander.diasbrito@gmail.com>
2018-11-21 21:59:45 -0200

    Adicionando senha a Cliente

--- src/main/java/com/leckbrito/cursomc/services/DBService.java
+++ src/main/java/com/leckbrito/cursomc/services/DBService.java
@@ -5,6 +5,7 @@ import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.leckbrito.cursomc.domain.Categoria;
@@ -32,6 +33,9 @@ import com.leckbrito.cursomc.repositories.ProdutoRepository;
 
 @Service
 public class DBService {
+	
+	@Autowired
+	private BCryptPasswordEncoder pe;
 	
 	@Autowired
 	private CategoriaRepository categoriaRepository;
@@ -112,7 +116,7 @@ public class DBService {
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 		
 		//Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "36378912377", TipoCliente.PESSOAFISICA);
-		Cliente cli1 = new Cliente(null, "Maria Silva", "alexander.diasbrito@gmail.com", "36378912377", TipoCliente.PESSOAFISICA);
+		Cliente cli1 = new Cliente(null, "Maria Silva", "alexander.diasbrito@gmail.com", "36378912377", TipoCliente.PESSOAFISICA, pe.encode("123") );
 		
 		cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
 		
