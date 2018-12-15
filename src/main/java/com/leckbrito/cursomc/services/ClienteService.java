ca60ccd2a713a4200b16062d4ebce37866c4c6b1
Alexander Dias Brito <alexander.diasbrito@gmail.com>
2018-12-15 17:19:42 -0200

    Enviando imagem via endpoint

--- src/main/java/com/leckbrito/cursomc/services/ClienteService.java
+++ src/main/java/com/leckbrito/cursomc/services/ClienteService.java
@@ -1,5 +1,6 @@
 package com.leckbrito.cursomc.services;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -11,6 +12,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.leckbrito.cursomc.domain.Cidade;
 import com.leckbrito.cursomc.domain.Cliente;
@@ -37,6 +39,9 @@ public class ClienteService {
 	
 	@Autowired
 	private BCryptPasswordEncoder pe; 
+	
+	@Autowired
+	private S3Service S3Service;
 	
 	public Cliente find(Integer id) {
 		
@@ -108,4 +113,9 @@ public class ClienteService {
 		newObj.setNome(obj.getNome());
 		newObj.setEmail(obj.getEmail());
 	}
+	
+	public URI uploadProfilePicture(MultipartFile multipartFile) {
+		return S3Service.uploadFile(multipartFile);
+	}
+	
 }
